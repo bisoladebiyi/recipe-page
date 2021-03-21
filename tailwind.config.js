02888e00256413be8bb483208384e7b98fa9d1dc
abisola <abisola.ca@gmail.com>
2021-03-22 00:53:30 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          100: "#c5d7bd",
+          200: "#9fb8ad"
+        }
+      },
+      fontFamily: {
+        rock: ["RocknRoll One"]
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
